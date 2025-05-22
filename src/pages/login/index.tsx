d2af5f47f3bb762abb8f30596d54e25c3d22e490
mahdisgb <elmahdifraoucene@gmail.com>
2025-05-22 14:17:05 +0100

    add login bg

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,10 +17,19 @@ export const Login = () => {
   
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen flex flex-col items-center justify-center gap-10 bg-gray-50 py-12 px-4 sm:px-6 lg:px-8"
+    style={{
+      // background: "linear-gradient(#1e55a9 ,#fff)",
+      background: "url('/bg.jpg')",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+    }}
+    >
+      <h1 className="font-bold text-center text-white" style={{fontSize:"80px"}}>Welcome to Wajihni</h1>
       <div className="max-w-md w-full space-y-8">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-200">
             {formType === "login" ? t('auth.loginTitle') : t('auth.registerTitle')}
           </h2>
         </div>
@@ -31,10 +40,10 @@ export const Login = () => {
             onChange={(e) => setFormType(e.target.value)}
             className="w-full"
           >
-            <Radio.Button value="login" className="w-1/2 text-center">
+            <Radio.Button value="login" className="w-1/2 text-center text-lg">
               {t('auth.login')}
             </Radio.Button>
-            <Radio.Button value="register" className="w-1/2 text-center">
+            <Radio.Button value="register" className="w-1/2 text-center text-lg">
               {t('auth.register')}
             </Radio.Button>
           </Radio.Group>
